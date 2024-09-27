d5817d7c953437525fe158f570fcb6c522dfd790
Blackalf14 <lucas.art7938@gmail.com>
2024-09-27 14:05:47 +0200

    T3/4 Jetpack recipes fix

--- kubejs/server_scripts/mods/Simply Jetpacks/SJRecipes.js
+++ kubejs/server_scripts/mods/Simply Jetpacks/SJRecipes.js
@@ -26,12 +26,14 @@ ServerEvents.recipes(event => {
 
     //Darkened
     event.replaceInput({output:'simplyjetpacks:jetpack_te3'},'thermal:energy_cell','thermal:upgrade_augment_2')
+    event.replaceInput({output:'simplyjetpacks:jetpack_te3'},'thermal:electrum_ingot','extendedcrafting:black_iron_ingot')
     thruster('3', 'extendedcrafting:black_iron_block', 'enderio:double_layer_capacitor', 'thermal:dynamo_stirling')
     dynamo('thermal:dynamo_stirling', 'extendedcrafting:black_iron_slate', 'enderio:dark_bimetal_gear')
     armored('3')
 
     //Draconic
     event.replaceInput({output:'simplyjetpacks:jetpack_te4'},'thermal:energy_cell','thermal:upgrade_augment_3')
+    event.replaceInput({output:'simplyjetpacks:jetpack_te4'},'thermal:enderium_ingot','thermal_extra:dragonsteel_ingot')
     thruster('4', 'thermal_extra:dragonsteel_block', 'enderio:octadic_capacitor', 'thermal:dynamo_lapidary')
     dynamo('thermal:dynamo_lapidary', 'thermal_extra:dragonsteel_plate', 'thermal:netherite_gear')
     armored('4')
