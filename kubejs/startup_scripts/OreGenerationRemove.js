3864441a925eb461be96520388a25e33e124b5c7
Blackalf14 <lucas.art7938@gmail.com>
2024-10-05 14:38:56 +0200

    General update

--- kubejs/startup_scripts/OreGenerationRemove.js
+++ kubejs/startup_scripts/OreGenerationRemove.js
@@ -2,6 +2,6 @@ WorldgenEvents.remove(event => {
 
     event.removeOres(props => {
 
-      props.blocks = ['elementalcraft:inert_crystal_ore', 'elementalcraft:deepslate_inert_crystal_ore', /rftoolsbase:dimensionalshard_.+/, /draconicevolution:.+draconium_ore/, 'thermal:ruby_ore', 'thermal:deepslate_ruby_ore', 'thermal:sapphire_ore', 'thermal:deepslate_sapphire_ore', 'bigreactors:yellorite_ore', 'bigreactors:deepslate_yellorite_ore', 'bigreactors:benitoite_ore', 'bigreactors:anglesite_ore', 'forbidden_arcanus:xpetrified_ore', 'forbidden_arcanus:stella_arcanum']
+      props.blocks = ['elementalcraft:inert_crystal_ore', 'elementalcraft:deepslate_inert_crystal_ore', /rftoolsbase:dimensionalshard_.+/, /draconicevolution:.+draconium_ore/, 'thermal:ruby_ore', 'thermal:deepslate_ruby_ore', 'thermal:sapphire_ore', 'thermal:deepslate_sapphire_ore', 'bigreactors:yellorite_ore', 'bigreactors:deepslate_yellorite_ore', 'bigreactors:benitoite_ore', 'bigreactors:anglesite_ore', 'forbidden_arcanus:xpetrified_ore', 'forbidden_arcanus:stella_arcanum', 'actuallyadditions:black_quartz_ore']
     })
 })
